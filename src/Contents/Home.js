2d3ad82c7b4453c7c080d26d0d2cb8ef00c0bb45
Benj Riethmuller <benjriethmuller@gmail.com>
2021-01-27 21:47:16 +0800

    homepage gif

--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-import Homepage from '../Img/Home/Homepage.gif';
+// import Homepage from '../Img/Home/Homepage.gif';
+import homegif from '../Img/Home/homegif.gif';
 
 class Home extends Component {
 
     render() {
         return(
             <div className="content-div">
-                <img src={Homepage} alt="" className="home-image"></img>
+                {/* <img src={Homepage} alt="" className="home-image"></img> */}
+                <img src={homegif} alt="" className="home-image"></img>
             </div>
         )
     }
