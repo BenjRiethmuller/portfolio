c2c30788447d66eab6901b79363a10db152b24a8
Benj Riethmuller <benjriethmuller@gmail.com>
2021-02-08 22:40:33 +0800

    menu responsiveness for mobile and tablet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import BeanOut from './Contents/BeanOut.js';
 import Bikeabout from './Contents/Bikeabout.js';
 import ConfideInThem from './Contents/ConfideInThem.js';
 import Cphber from './Contents/Cphber.js';
-import Gravy from './Contents/Gravy.js';
+// import Gravy from './Contents/Gravy.js';
 import Info from './Contents/Info.js';
 import Jamable from './Contents/Jamable.js';
 import Leelo from './Contents/Leelo.js';
@@ -26,7 +26,7 @@ class App extends Component {
           <Route path="/bikeabout"> <Bikeabout /> </Route>
           <Route path="/confide-in-them"> <ConfideInThem /> </Route>
           <Route path="/cphber"> <Cphber /> </Route>
-          <Route path="/gravy-magazine"> <Gravy /> </Route>
+          {/* <Route path="/gravy-magazine"> <Gravy /> </Route> */}
           <Route path="/info"> <Info /> </Route>
           <Route path="/jamable"> <Jamable /> </Route>
           <Route path="/leelo-music"> <Leelo /> </Route>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,39 @@ import { Link } from "react-router-dom";
 import Navitem from '../NavItem/NavItem';
 import './Navbar.css';
 
+const expandDesignMenu = () => {
+    let designMenu = document.querySelector('.design-dropdown-content');
+    let display = designMenu.style.display;
+
+        if (display === 'none') {
+            designMenu.style.display = 'block';
+          } else {
+            designMenu.style.display = 'none';
+          };
+  };
+
+  const expandPhotoMenu = () => {
+    let photoMenu = document.querySelector('.photo-dropdown-content');
+    let display = photoMenu.style.display;
+
+        if (display === 'none') {
+            photoMenu.style.display = 'block';
+          } else {
+            photoMenu.style.display = 'none';
+          };
+  };
+
+  const expandInfoMenu = () => {
+    let infoMenu = document.querySelector('.info-dropdown-content');
+    let display = infoMenu.style.display;
+
+        if (display === 'none') {
+            infoMenu.style.display = 'block';
+          } else {
+            infoMenu.style.display = 'none';
+          };
+  };
+
 class Navbar extends Component {
     constructor(props)
     {
@@ -11,6 +44,7 @@ class Navbar extends Component {
             'NavItemActive':''
         }
     }
+
     activeitem=(x)=>
     {
         if(this.state.NavItemActive.length>0){
@@ -20,9 +54,12 @@ class Navbar extends Component {
             document.getElementById(this.state.NavItemActive).classList.add('active');
         });
     };
+    
 
     render() {
         return (
+            <div>
+            <div className="desktop-nav">
             <nav>
             <ul>
             <Link className="site-header" to="/" >Benj <br /> Riethmuller</Link>
@@ -30,7 +67,7 @@ class Navbar extends Component {
             <h3 className="nav-header">DESIGN</h3>
             <Navitem item="Jamable" tolink="/jamable" activec={this.activeitem} ></Navitem>
             <Navitem item="Bikeabout" tolink="/bikeabout" activec={this.activeitem}></Navitem>
-            <Navitem item="Gravy Magazine" tolink="/gravy-magazine" activec={this.activeitem}></Navitem>
+            {/* <Navitem item="Gravy Magazine" tolink="/gravy-magazine" activec={this.activeitem}></Navitem> */}
             <Navitem item="Leelo" tolink="/leelo-music" activec={this.activeitem}></Navitem>
             <h3 className="nav-header">PHOTO</h3>
             <Navitem item="CPHBER" tolink="/cphber" activec={this.activeitem} ></Navitem>
@@ -41,6 +78,34 @@ class Navbar extends Component {
             <Navitem item="About & Contact" tolink="/info" activec={this.activeitem}></Navitem>
             </ul>
             </nav>
+            </div>
+
+            {/* for mobile */}
+                <div className="mobile-nav">
+                    <nav>
+                        <ul>
+                        <Link className="site-header" to="/" >Benj <br /> Riethmuller</Link>
+                        <h3 className="nav-header design" onClick={expandDesignMenu}>DESIGN</h3>
+                            <div className="design-dropdown-content">
+                                <Navitem item="Jamable" tolink="/jamable" activec={this.activeitem} ></Navitem>
+                                <Navitem item="Bikeabout" tolink="/bikeabout" activec={this.activeitem}></Navitem>
+                                <Navitem item="Leelo" tolink="/leelo-music" activec={this.activeitem}></Navitem>
+                            </div>
+                        <h3 className="nav-header photo" onClick={expandPhotoMenu}>PHOTO</h3>
+                            <div className="photo-dropdown-content">
+                                <Navitem item="CPHBER" tolink="/cphber" activec={this.activeitem} ></Navitem>
+                                <Navitem item="Confide In Them" tolink="/confide-in-them" activec={this.activeitem}></Navitem>
+                                <Navitem item="Let's Bean Out Tomorrow" tolink="/lets-bean-out-tomorrow" activec={this.activeitem}></Navitem>
+                                <Navitem item="Read The Bible" tolink="/read-the-bible" activec={this.activeitem}></Navitem>
+                            </div>
+                        <h3 className="nav-header info" onClick={expandInfoMenu}>INFO</h3>
+                            <div className="info-dropdown-content">
+                                <Navitem item="About & Contact" tolink="/info" activec={this.activeitem}></Navitem>
+                            </div>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
             )
         }
     }
